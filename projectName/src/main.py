b6a48c2a19ece4f918f8bb7eadcc2b6f6d8122fb
kvanderm <kv@class-docker>
2025-01-29 08:07:59 +0000

    Fixed version.md again x2

--- projectName/src/main.py
+++ projectName/src/main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
+import datetime
+print(datetime.datetime.today())
 
-f = open('/workspace/version-control/projectName/output/version.md','w')
-f.write(str(time.time()) + '\n')
-f.close()
+#f = open('/workspace/version-control/projectName/output/version.md','w')
+#f.write(str(time.time()) + '\n')
+#f.close()
